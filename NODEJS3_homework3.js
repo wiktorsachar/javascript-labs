732a17f57639ff287a212c3721ddc94c3abcdd3a
wiktorsachar <44320437+wiktorsachar@users.noreply.github.com>
2018-11-29 19:34:09 +0100

    Add files via upload

---
+++ NODEJS3_homework3.js
@@ -0,0 +1,10 @@
+/*Bazując na zadaniu 2 zabezpieczmy naszą aplikację tak aby w przypadku błędu
+odczytu pliku(zła nazwa pliku/inny rodzaj błędu) poinformowała użytkownika o
+problemie w konsoli.*/
+
+const fs = require('fs');
+
+fs.readFile('NODEJS3_homework1_output.json', (err, data) => {
+    if (err) throw err;
+    console.log(JSON.parse(data).lastname);
+  });
