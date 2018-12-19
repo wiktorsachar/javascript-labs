a1a3c94f7b7b5f80658953187563bf9bb9e27eda
wiktorsachar <44320437+wiktorsachar@users.noreply.github.com>
2018-12-19 13:09:43 +0100

    Delete problem.js

--- problem.js
+++
@@ -1,9 +0,0 @@
-let tab = [
-    {
-        x: 1,
-        y: 'object one value',
-        operation: () => 'object one prafix' + this.x + this.y
-    }
-]
-
-console.log(tab[0].operation()) // logs "object one prafixundefinedundefined" - dlaczego?
