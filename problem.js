7b165db56c5c21272105af7ec6c05bea366b70f0
wiktorsachar <44320437+wiktorsachar@users.noreply.github.com>
2018-12-27 16:41:22 +0100

    Add files via upload

---
+++ problem.js
@@ -0,0 +1,17 @@
+const axios = require('axios');
+
+
+async function getUser(id) {
+   try {
+    const url = `https://jsonplaceholder.typicode.com/users/${id}/`;
+    await axios.get(url)
+    /*.then((result) => console.log(result.data.name))*/
+    .then((result) => {
+        return result.data.name; //czemu to nie dziala? jak zrobic zeby zwracalo wartosc, a nie tylko wyswietlalo w konsoli?
+    })
+    } catch(error) {
+     console.log(error.response.statusText);
+    }
+};
+
+console.log(getUser(4))
