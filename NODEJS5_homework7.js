d33fbbe380e38cc371d330a6eff1f47f44cb7899
wiktorsachar <44320437+wiktorsachar@users.noreply.github.com>
2019-01-08 21:33:39 +0100

    Add files via upload

---
+++ NODEJS5_homework7.js
@@ -0,0 +1,19 @@
+/*Wykorzystując wiedzę z poprzednich zajęć użyjmy zewnętrznej biblioteki axios i pobierzmy
+użytkownika dane wykorzystując składnię async/await.
+Endpoint do użytkownika: https://jsonplaceholder.typicode.com/users/2*/
+
+const axios = require('axios');
+
+const id = process.argv[2];
+
+async function getUser() {
+   try {
+    const url = `https://jsonplaceholder.typicode.com/users/${id}/`;
+    await axios.get(url)
+    .then((result) => console.log(result.data.name));
+   } catch(error) {
+     console.log(error.response.statusText);
+   }
+};
+
+getUser();
